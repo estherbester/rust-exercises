0be66b91266d763240021f9ad0dec53c0175daed
Esther N <estherbester@users.noreply.github.com>
2019-07-19 09:22:04 -0700

    Update lib.rs

--- problems/problem5/src/lib.rs
+++ problems/problem5/src/lib.rs
@@ -28,31 +28,12 @@ fn compound_formula(principal: i64, interest_rate: f64) -> i64 {
     
 impl Mortgage {
     // 2. Write the constructor
-    pub fn new(
-        original_principal: i64, 
-        interest_rate: f64, 
-        loan_term_months: i32
-    ) -> Mortgage {
-        Mortgage {
-            original_principal,
-            interest_rate,
-            loan_term_months,
-        }
+    pub fn new() {
     }
 
     // 3. Calculate remaining principal after n months
     fn principal_remaining(&self, months: i32, payment: i64) -> i64 {
-        let mut remaining_principal = self.original_principal;
-        for month in (0..months) { 
-            remaining_principal = compound_formula(remaining_principal, self.interest_rate);
-            remaining_principal = remaining_principal - payment;
-            remaining_principal = match remaining_principal.cmp(&0) { 
-                Ordering::Greater => remaining_principal,
-                _ => 0,
-            };
-            println!("Month {}: ${}", month + 1, remaining_principal);
-        } 
-        remaining_principal
+        100
     }
     
     // 4. Calculate minimum monthly payment required to pay the loan off on time.
@@ -67,14 +48,7 @@ impl Mortgage {
     
     // 6. Calculate time needed to pay off the loan given a monthly payment of x dollars
     fn months_until_payoff(&self, monthly_payment: i64) -> i32 {
-        let mut months: i32 = 0;
-        let mut new_principal = self.original_principal;
-        while new_principal >= 0 {
-            new_principal = compound_formula(new_principal, self.interest_rate);
-            new_principal = new_principal - monthly_payment;
-            months = months + 1;
-        } 
-        months 
+        12
     } 
 }
 
